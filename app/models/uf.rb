cda16f4873038f650efa4de0d12fdac9e40cbae9
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-30 14:17:13 -0400

    add: controllers

--- app/models/uf.rb
+++ app/models/uf.rb
@@ -1,2 +1,14 @@
 class Uf < ApplicationRecord
+    def self.load_csv_data
+        file = File.open('tmp/uf.csv')
+        file_data = file.readlines.map(&:chomp)
+        file_data.each_with_index do |row,index|
+            next if index.zero?
+            cols = row.split(",")
+            Uf.create(date:"2020/01/#{cols[0]}", value:cols[1])
+            if index < 29
+                Uf.create(date:"2020/02/#{cols[0]}", value:cols[2])
+            end
+        end
+    end
 end
